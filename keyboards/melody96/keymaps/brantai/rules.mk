6fa215c3ab7d1ee2bd7ad4f6ea840311cadd276d
Brent Naylor <bdnaylor@gmail.com>
2019-01-09 14:49:58 -0800

    bugfixes
    for emoji picker key and screen lock macro

--- keyboards/melody96/keymaps/brantai/rules.mk
+++ keyboards/melody96/keymaps/brantai/rules.mk
@@ -1,4 +1,4 @@
 MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no
-KEY_LOCK_ENABLE = yes # Enable keylock
+KEY_LOCK_ENABLE = no # Enable keylock
