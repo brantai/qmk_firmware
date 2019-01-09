74fdd6f782ea0cce3821c11ba0fecc151ae860eb
Brent Naylor <brantai@gmail.com>
2019-01-08 19:30:26 -0800

    Oops, there is no mac 'stop' key

--- keyboards/melody96/keymaps/brantai/keymap.c
+++ keyboards/melody96/keymaps/brantai/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#define KC_MCTL LCTL(KC_UP)
 
 enum custom_keycodes {
     PLACEHOLDER = SAFE_RANGE,
@@ -28,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Layer 0, default layer
 ____________________________________________________________________________________________________________________________________________________________________________
 |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
-| ESC*   |   F1   |   F2   |   F3   |   F4   |  MRWD  |  MPLY  |  MSTP  |  MFFFD |   F9   |  F10   |  F14   |  F15   |  MUTE  | VOLDWN | VOLUP  |  HOME  |  END   |LCK_SCR |
+| ESC*   |   F1   |   F2   |   F3   |   F4   |  MRWD  |  MPLY  |  MFFD  |KC_MCTL |   F9   |  F10   |  F14   |  F15   |  MUTE  | VOLDWN | VOLUP  |  HOME  |  END   |LCK_SCR |
 |________|________|________|________|________|________|________|________|________|________|________|________|________|________|________|________|________|________|________|
 |        |        |        |        |        |        |        |        |        |        |        |        |        |        | BACK   |        |        |        |        |
 |  ~`    |   1    |   2    |   3    |   4    |   5    |   6    |   7    |   8    |   9    |   0    |  _ -   | =  +   |   \    | SPACE  |    +   |   /    |  *     |   -    |
@@ -50,7 +51,7 @@ ________________________________________________________________________________
 
 
 	LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_MRWD,   KC_MPLY,   KC_MSTP,   KC_MFFD,   KC_F9,   KC_F10,  KC_F14,  KC_F15,  KC__MUTE, KC__VOLDOWN, KC__VOLUP,  KC_HOME, KC_END, M(LOCK_SCREEN),
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_MRWD,   KC_MPLY,   KC_MFFD,   KC_MCTL,   KC_F9,   KC_F10,  KC_F14,  KC_F15,  KC__MUTE, KC__VOLDOWN, KC__VOLUP,  KC_HOME, KC_END, M(LOCK_SCREEN),
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC, KC_PPLS, KC_PSLS, KC_PAST, KC_PMNS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_7,   KC_8,   KC_9,   KC_PGUP,
     MO(1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_4,   KC_5,   KC_6,   KC_PGDN,
